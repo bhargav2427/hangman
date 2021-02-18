3f799b8c22bd7c3a0c9929e150dbc62b61d163f0
bhargav2427 <akhani.bharga@gmail.com>
2021-02-18 16:02:22 +0530

    disabled added

--- main.js
+++ main.js
@@ -2,8 +2,17 @@ var i = 97;
 var buttons = document.getElementById("buttons");
 while (i < 123) {
   var node = document.createElement("div");
+  node.id = String.fromCharCode(i);
   var textNode = document.createTextNode("" + String.fromCharCode(i));
   node.appendChild(textNode);
   document.getElementById("buttons").appendChild(node);
+  node.addEventListener("click", function () {
+    clicked(this);
+  });
   i++;
+}
+
+function clicked(x) {
+  console.log(x.id);
+  document.getElementById(x.id).classList.add("disabled");
 }
